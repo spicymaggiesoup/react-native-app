c0628ec8aaf360191425916ac8486408cab1597a
spicymaggiesoup <samantha8009@gamil.com>
2022-06-28 21:31:59 +0900

    [Modify] move toolbar to index.js

--- src/Index.js
+++ src/Index.js
@@ -1,31 +1,41 @@
 import React, { useRef, useState } from "react";
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, DrawerLayoutAndroid, View, ScrollView } from 'react-native';
+import { StyleSheet, DrawerLayoutAndroid, View, Text, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Login from './screens/Login';
-import Toolbar from './screens/Toolbar';
 import Menu from './screens/Menu';
 
 const App = () => {
   
+  const titleText = "COWKITE 2022";
   const drawer = useRef(null);
-  const [drawerPosition, setDrawerPosition] = useState("left");
+  const [ drawerPosition ] = useState("left");
 
   return (
-    // <ScrollView>
-      <DrawerLayoutAndroid
-        ref={drawer}
-        drawerWidth={300}
-        drawerPosition={drawerPosition}
-        renderNavigationView={Menu}
-      >
-        <View style={styles.container}>
-          <Toolbar/>
-          <Login/>
-        </View>
-      </DrawerLayoutAndroid>
-    // </ScrollView>
-  );
+    // <ScrollView />
+    <DrawerLayoutAndroid
+      ref={drawer}
+      drawerWidth={300}
+      drawerPosition={drawerPosition}
+      renderNavigationView={Menu}
+    >
+      <View class="container" style={styles.container}>
+          <View style={styles.toolbar}>
+            <Icon.Button
+              name="menu"
+              size={30}
+              color={styles.menuButton.color}
+              style={styles.menuButton}
+              onPress={() => drawer.current.openDrawer()}
+            >
+            {titleText}
+            </Icon.Button>
+          </View>
+        <Login/>
+      </View>
+    </DrawerLayoutAndroid>
+  ); 
 }
 
 const styles = StyleSheet.create({
@@ -34,6 +44,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingTop: 50,
   },
+  toolbar: {
+  },
+  menu: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 16
+  },
+  title: {
+    color: '#35363A',
+    fontSize: 20,
+  },
+  menuButton: {
+    color: '#A6A6A6',
+    backgroundColor: '#fff',
+  }
 });
 
 export default App;
