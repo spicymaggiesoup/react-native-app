361d6ca6834d603383bed76f0c6d8386dd48f0f1
spicymaggiesoup <samantha8009@gmail.com>
2022-06-29 22:40:54 +0900

    [Add] redux for createStore

--- src/Index.js
+++ src/Index.js
@@ -1,22 +1,17 @@
-import React from 'react';
-// import { Provider } from 'react-redux';
+import React, { createContext } from 'react';
+import { Provider } from 'react-redux';
 // import { configureStore, applyMiddleware } from 'redux';
 // import ReduxThunk from 'redux-thunk';
 // import store from './store';
 import Main from './screens/Main';
+import store from './Context/store';
+// import Provider from './Context/Provider';
 
 const App = () => {
-
-  // const store = configureStore({
-  //   reducer: {
-  //     counterReducer: counterReducer,
-  //   },
-  // });
-
   return (
-    // <Provider store={store}>
-    <Main/>
-    // </Provider>
+    <Provider store={store}>
+      <Main/>
+    </Provider>
   );
 };
 
